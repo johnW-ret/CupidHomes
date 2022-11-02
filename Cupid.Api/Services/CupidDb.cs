2a663e07f1e3411242e177e4ea8acb8e8c695431
johnW_ret <retruate@gmail.com>
2022-11-01 19:26:27 -0500

    Added initial DbContext

---
+++ Cupid.Api/Services/CupidDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cupid.Models;
+
+namespace Cupid.Api;
+
+public class CupidDb : DbContext
+{
+    public CupidDb(DbContextOptions<CupidDb> options)
+        : base(options) { }
+    // public DbSet<T> TSet => Set<T>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //new TEntityTypeConfiguration().Configure(modelBuilder.Entity<T>());
+    }
+}
