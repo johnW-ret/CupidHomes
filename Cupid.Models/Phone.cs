58aa2de40c3e65d4c2d580c88a1d427d75ce09b3
johnW_ret <retruate@gmail.com>
2022-11-10 20:52:18 -0600

    Delete everything

--- Cupid.Models/Phone.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cupid.Models;
-
-public class Phone
-{
-    public string? AreaCode { get; set; }
-    public string Number { get; set; } = default!;
-}
--- Cupid.Models/Plan.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cupid.Models;
-
-public class Plan
-{
-    public int Number { get; set; }
-    public string? Name { get; set; }
-    public DateTimeOffset? RetiredOn { get; set; }
-}
